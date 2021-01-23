2913df02dfb76e18e845bf80b2d1913ddb9a7ce0
Redstone Zhao <13740080@qq.com>
2021-01-23 19:48:57 +0800

    修正cnf上的变量名称 sequelize 而不是 dbs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function Main(cnf, deps) {
   // 这里之所以要注入 Sequelize 是为了保证项目自身可以灵活选择自己的 Sequelize 版本, 这样改公共模块就会更加稳定, 避免频繁升级
   const { Sequelize } = deps;
-  const { dbs } = cnf;
+  const { sequelize: dbs } = cnf;
   const sequelizes = {};
   for (const k of Object.keys(dbs)) {
     const db = dbs[k];
